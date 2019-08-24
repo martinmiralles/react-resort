ef481fd8ee29f015fcd6b7c365a63ffd84b1e738
martinmiralles <martinmiralles66@gmail.com>
2019-08-24 16:30:11 -0400

    Navbar + Hero + Banners + Services

--- src/App.js
+++ src/App.js
@@ -3,16 +3,23 @@ import "./App.css";
 
 import Home from "./pages/Home";
 import Rooms from "./pages/Rooms";
-import SingleRoom from "./pages/SingleRoomme";
+import SingleRoom from "./pages/SingleRoom";
 import Error from "./pages/Error";
+
+import { Route, Switch } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
-      <Home></Home>
-      <Rooms></Rooms>
-      <SingleRoom></SingleRoom>
-      <Error></Error>
+      <Navbar></Navbar>
+      <Switch>
+        <Route exact path='/' component={Home}></Route>
+        <Route exact path='/rooms/' component={Rooms}></Route>
+        <Route exact path='/rooms/:slug' component={SingleRoom}></Route>
+        <Route component={Error}></Route>
+      </Switch>
     </>
   );
 }
